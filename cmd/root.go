e691ff901212089f95d373c3f263372d5ed32a6e
Parham Alvani <parham.alvani@gmail.com>
2020-02-04 12:02:14 +0330

    feat: Add Root Command

--- cmd/root.go
+++ cmd/root.go
@@ -4,19 +4,21 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/parsaaes/netwolf/config"
 	"github.com/spf13/cobra"
 )
 
 // ExitFailure status code
 const ExitFailure = 1
 
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	cfg := config.Init("config.yaml")
+
 	var root = &cobra.Command{
 		Use:   "nw",
 		Short: "netwolf",
-		Run: func(cmd *cobra.Command, args []string) {
-
-		},
 	}
 
 	if err := root.Execute(); err != nil {
